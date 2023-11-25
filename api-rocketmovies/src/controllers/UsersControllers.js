8e504f6e37ed9d04c023128a0b7ff28b0fe961aa
Matheus <matheustorresdev@gmail.com>
2023-11-25 19:44:50 -0300

    Configurando DB e Criando tabelas

--- api-rocketmovies/src/controllers/UsersControllers.js
+++
@@ -1,6 +0,0 @@
-class UserControllers {
-    async create(req,res) {
-        const { name, email, password } = request.body;
-        
-    }
-}
--- api-rocketmovies/src/routes/users.routes.js
+++ api-rocketmovies/src/routes/users.routes.js
@@ -1,2 +1,11 @@
-const { Router } = require("express");
-const routes = Router();
+const { Router } = require('express')
+
+const UsersController = require('../controllers/UsersController')
+
+const usersRoutes = Router()
+
+const usersController = new UsersController()
+
+usersRoutes.post('/', usersController.create)
+
+module.exports = usersRoutes
