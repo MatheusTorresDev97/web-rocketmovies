ca7c96aa8f08018c56e0f5af637199216e7b1638
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-27 09:31:01 -0300

    Finalizando Cabecalho

--- app-rocketmovies/src/components/Header/index.jsx
+++ app-rocketmovies/src/components/Header/index.jsx
@@ -1,30 +1,46 @@
-import React from 'react'
 import { Link, useNavigate } from "react-router-dom";
 import { Container, Profile, Logout } from "./styles";
 import { Wrapper } from "../Wrapper";
+import { useAuth } from "../../hooks/auth";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
+import { api } from "../../services/api";
 
-const Header = ({children}) => {
+// eslint-disable-next-line react/prop-types
+const Header = ({ children }) => {
+  const { user, signOut } = useAuth();
+
+  const navigate = useNavigate();
+
+  const avatar = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+
+  function handleSignOut() {
+    navigate("/");
+    signOut();
+  }
+
   return (
     <Container>
-        <Wrapper>
-        <Link to="/" className="only-in-desktop">
-          <h2>RocketMovies</h2>
+    <Wrapper>
+      <Link to="/" className="only-in-desktop">
+        <h2>RocketMovies</h2>
+      </Link>
+      {children}
+      <Profile>
+        <div>
+          <p>{user.name}</p>
+          <Logout type="button" onClick={handleSignOut}>
+            Sair
+          </Logout>
+        </div>
+        <Link to="/profile">
+          <img src={avatar} alt={`Foto de ${user.name}`} />
         </Link>
-        {children}
-        <Profile>
-            <div>
-                <p>Usuario</p>
-                <Logout type="button">
-                    Sair
-                </Logout>
-            </div>
-            <Link to="/profile">
-                
-            </Link>
-        </Profile>
-        </Wrapper>
-    </Container>
-  )
-}
+      </Profile>
+    </Wrapper>
+  </Container>
+  );
+};
 
-export default Header
+export default Header;
