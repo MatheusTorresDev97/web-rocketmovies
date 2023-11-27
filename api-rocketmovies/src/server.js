efd2d26de91593ed6de48ffb72de09447e57045a
ROBERTO MATHEUS LIMA TORRES <07121450526@pmse.gov>
2023-11-27 10:13:23 -0300

    variaveis de segurança

--- api-rocketmovies/src/server.js
+++ api-rocketmovies/src/server.js
@@ -1,4 +1,5 @@
 require('express-async-errors')
+require("dotenv/config")
 const express = require('express')
 const cors = require("cors");
 
@@ -36,5 +37,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.SERVER_PORT || 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
